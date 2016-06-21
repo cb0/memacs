9b17f3c1ae554ec3b1e96e159143465fb05955a2
Marcus Puchalla <marcus.puchalla@googlemail.com>
2016-06-21 11:57:40 +0200

    Add "M-," for commenting out lines.

--- emacs.d/mymacs/init.el
+++ emacs.d/mymacs/init.el
@@ -570,6 +570,16 @@
 ;; commenting
 (global-set-key (kbd "C-x C-;") 'comment-region)
 (global-set-key (kbd "C-x C-:") 'uncomment-region)
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+
+(global-set-key (kbd "M-,") 'comment-or-uncomment-region-or-line)
 
 ;;imenu for fast infile naviation
 (global-set-key (kbd "M-i") 'imenu)
