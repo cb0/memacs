d7f7de3026d2e7e4339665f41ad13d7cce2b321f
Marcus Puchalla <marcus.puchalla@googlemail.com>
2016-09-27 14:41:42 +0200

    Reset multi-cursors to default key bindings.

--- emacs.d/mymacs/init.el
+++ emacs.d/mymacs/init.el
@@ -888,7 +888,7 @@ BEG and END (region to sort)."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (package-require 'multiple-cursors)
 (require 'multiple-cursors)
-(global-set-key (kbd "C-M-m M-m") 'mc/edit-lines)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
