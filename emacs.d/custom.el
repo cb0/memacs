b7e534eccbfe283345c9d0792a8505f3eb7c710d
Marcus Puchalla <marcus.puchalla@googlemail.com>
2023-02-11 16:10:10 +0100

    Add customs file

---
+++ emacs.d/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (list org-directory))
+ '(org-agenda-ndays 7)
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-start-on-weekday nil)
+ '(org-deadline-warning-days 14)
+ '(org-directory "~/org/journal/")
+ '(org-fast-tag-selection-single-key 'expert)
+ '(org-reverse-note-order t)
+ '(package-selected-packages
+   '(zoom-window zenburn-theme yasnippet xml-rpc workgroups2 wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smex smartparens slime simple-httpd project-explorer pinentry persp-projectile org-wc org-super-agenda org-journal org-caldav oauth2 multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
