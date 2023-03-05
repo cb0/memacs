746d189a169e209622b4ed4e9540ea61cd7e34c1
Marcus Puchalla <marcus.puchalla@tastenkunst.io>
2023-03-05 19:07:54 +0100

    Merge os x file. Probably I now need to adjust linux again.
    Need to find the time to make them more system specific.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (list org-directory))
+ '(org-agenda-files nil)
  '(org-agenda-ndays 7)
  '(org-agenda-show-all-dates t)
  '(org-agenda-skip-deadline-if-done t)
@@ -14,7 +14,7 @@
  '(org-fast-tag-selection-single-key 'expert)
  '(org-reverse-note-order t)
  '(package-selected-packages
-   '(zoom-window zenburn-theme yasnippet xml-rpc workgroups2 wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smex smartparens slime simple-httpd project-explorer pinentry persp-projectile org-wc org-super-agenda org-journal org-caldav oauth2 multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm)))
+   '(slime zenburn yasnippet xml-rpc wakatime-mode w3m vuiet spray spaceline smex rainbow-mode org-wc org-super-agenda org-journal org-caldav org-ac notmuch nnhackernews multiple-cursors magit lsp-ui lsp-haskell langtool esup counsel company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
