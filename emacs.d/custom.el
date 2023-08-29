2c24399da53f01d1cd77eb96241097a1e52ffaa0
Marcus Puchalla <marcus.puchalla@googlemail.com>
2023-08-29 20:36:54 +0200

    local linux notebook changes

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -14,7 +14,7 @@
  '(org-fast-tag-selection-single-key 'expert)
  '(org-reverse-note-order t)
  '(package-selected-packages
-   '(slime zenburn yasnippet xml-rpc wakatime-mode w3m vuiet spray spaceline smex rainbow-mode org-wc org-super-agenda org-journal org-caldav org-ac notmuch nnhackernews multiple-cursors magit lsp-ui lsp-haskell langtool esup counsel company)))
+   '(zoom-window zenburn-theme yasnippet xml-rpc workgroups2 which-key wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smudge smex smartparens slime project-explorer pinentry persp-projectile org-wc org-super-agenda org-journal org-caldav multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm 2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
