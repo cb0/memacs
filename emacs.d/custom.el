fad87e9bae6ad9dd9d14c675e32ce44ea8e7aa3b
Marcus Puchalla <marcus.puchalla@googlemail.com>
2023-10-31 19:43:22 +0100

    org-roam + anki

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,18 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
+ '(org-agenda-files (list org-directory))
  '(org-agenda-ndays 7)
  '(org-agenda-show-all-dates t)
  '(org-agenda-skip-deadline-if-done t)
  '(org-agenda-skip-scheduled-if-done t)
  '(org-agenda-start-on-weekday nil)
  '(org-deadline-warning-days 14)
- '(org-directory "~/org/journal/")
+ '(org-directory "~/sync/org/journal/")
  '(org-fast-tag-selection-single-key 'expert)
  '(org-reverse-note-order t)
  '(package-selected-packages
-   '(zoom-window zenburn-theme yasnippet xml-rpc workgroups2 which-key wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smudge smex smartparens slime project-explorer pinentry persp-projectile org-wc org-super-agenda org-journal org-caldav multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm 2048-game)))
+   '(anki-editor zoom-window zenburn-theme yasnippet xml-rpc workgroups2 which-key wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smudge smex smartparens slime project-explorer pinentry persp-projectile org-wc org-super-agenda org-roam org-journal org-caldav multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm 2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
