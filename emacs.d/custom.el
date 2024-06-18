4c61bdb4b4d01395aa1300c2359b28cd1f832cb5
Marcus Puchalla <marcus.puchalla@googlemail.com>
2024-06-18 22:49:48 +0200

    add custom agenda files

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (list org-directory))
+ '(org-agenda-files
+   '("~/sync/org/org-roam/20240125144013-tastenkunst.org" "/home/cb0/sync/org/org-roam/20240515150139-my_first_mushrooms.org" "/home/cb0/sync/org/org-roam/20240611184210-upnext.org" "/home/cb0/sync/org/old/homenotes.org"))
  '(org-agenda-ndays 7)
  '(org-agenda-show-all-dates t)
  '(org-agenda-skip-deadline-if-done t)
@@ -14,7 +15,7 @@
  '(org-fast-tag-selection-single-key 'expert)
  '(org-reverse-note-order t)
  '(package-selected-packages
-   '(anki-editor zoom-window zenburn-theme yasnippet xml-rpc workgroups2 which-key wakatime-mode w3m vuiet use-ttf use-package sudo-edit stickyfunc-enhance spray spaceline smudge smex smartparens slime project-explorer pinentry persp-projectile org-wc org-super-agenda org-roam org-journal org-caldav multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex ag ac-helm 2048-game)))
+   '(zoom-window zenburn-theme yasnippet xml-rpc workgroups2 which-key wakatime-mode w3m vuiet use-ttf use-package transpose-frame sudo-edit stickyfunc-enhance spray spaceline smudge smex smartparens slime project-explorer pinentry persp-projectile org-wc org-super-agenda org-roam org-journal org-caldav multiple-cursors magit lsp-ui lsp-haskell langtool hydra helm-rhythmbox helm-ag exwm esup enwc counsel company-ghci auto-complete-pcmp auctex anki-editor ag ac-helm 2048-game)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
