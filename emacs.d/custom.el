03d1928592cea268d8c192a2f6c22cd421eb988f
Marcus Puchalla <marcus.puchalla@googlemail.com>
2024-08-18 15:10:50 +0200

    config: adjust path, change packages

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(exec-path
+   '("/usr/local/bin" "/usr/bin" "/var/lib/snapd/snap/bin" "/usr/lib/emacs/29.3/x86_64-pc-linux-gnu" "/usr/bin/mscgen" "/usr/bin/mscgen" "~/.local/bin/"))
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
  '(package-selected-packages
-   '(lsp-treemacs zoom-window zenburn-theme which-key web-server vertico ts-fold treesit-auto treemacs spaceline smex org-wc org-super-agenda org-caldav orderless nerd-icons multiple-cursors marginalia magit lsp-ui flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu company claude-shell anki-editor all-the-icons-dired all-the-icons-completion)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(modus-themes pomm winum focus lsp-mode npm-mode 0x0 consult-lsp lsp-consult vue-ts-mode lsp-tailwindcss zoom-window zenburn-theme which-key web-server vertico typescript-mode ts-fold treesit-auto spaceline smex org-wc org-super-agenda org-caldav orderless nerd-icons multiple-cursors marginalia magit lsp-ui lsp-treemacs json-mode flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu company claude-shell anki-editor all-the-icons-dired all-the-icons-completion))
+ '(package-vc-selected-packages
+   '((vue-ts-mode :vc-backend Git :url "https://www.github.com/8uff3r/vue-ts-mode"))))
