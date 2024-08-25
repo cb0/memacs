32da3f815b9e92d0a03dc48645ea586f2f665151
Marcus Puchalla <marcus.puchalla@googlemail.com>
2024-08-25 21:12:31 +0200

    minor changes wip

--- emacs
+++ emacs
@@ -1,12 +1,13 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (setenv "LSP_USE_PLISTS" "true")
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
                    user-emacs-directory))
--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,10 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(exec-path
-   '("/usr/local/bin" "/usr/bin" "/var/lib/snapd/snap/bin" "/usr/lib/emacs/29.3/x86_64-pc-linux-gnu" "/usr/bin/mscgen" "/usr/bin/mscgen" "~/.local/bin/"))
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
  '(package-selected-packages
-   '(modus-themes pomm winum focus lsp-mode npm-mode 0x0 consult-lsp lsp-consult vue-ts-mode lsp-tailwindcss zoom-window zenburn-theme which-key web-server vertico typescript-mode ts-fold treesit-auto spaceline smex org-wc org-super-agenda org-caldav orderless nerd-icons multiple-cursors marginalia magit lsp-ui lsp-treemacs json-mode flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu company claude-shell anki-editor all-the-icons-dired all-the-icons-completion))
- '(package-vc-selected-packages
-   '((vue-ts-mode :vc-backend Git :url "https://www.github.com/8uff3r/vue-ts-mode"))))
+   '(cape epa-file zoom-window zenburn-theme yasnippet winum which-key-posframe web-server vertico typescript-mode ts-fold treesit-auto spaceline smex pomm org-wc org-super-agenda org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu consult-lsp company claude-shell anki-editor all-the-icons-dired all-the-icons-completion 0x0)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:height 140)))))
