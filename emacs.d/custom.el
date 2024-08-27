4db688c1e35d059c242a50de97e3de6a36dc3c58
Marcus Puchalla <marcus.puchalla@googlemail.com>
2024-08-27 08:21:56 +0200

    wip: keepass integration
    For now I cannot use it, as when the KeePass popup, asking me if i
    want to allow the access to the secret service api, I cannot click or
    select anything in exwm. Not sure if it is related the the wm, as
    hovering the icons works.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
  '(package-selected-packages
-   '(cape epa-file zoom-window zenburn-theme yasnippet winum which-key-posframe web-server vertico typescript-mode ts-fold treesit-auto spaceline smex pomm org-wc org-super-agenda org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu consult-lsp company claude-shell anki-editor all-the-icons-dired all-the-icons-completion 0x0)))
+   '(exec-path-from-shell zoom-window zenburn-theme yasnippet winum which-key-posframe web-server vertico typescript-mode ts-fold treesit-auto spaceline smex pomm org-wc org-super-agenda org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard corfu consult-lsp company claude-shell chatgpt-shell cape anki-editor all-the-icons-dired all-the-icons-completion 0x0)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
