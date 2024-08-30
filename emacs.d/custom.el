0713efaeb3436e23e1bbc70711e748c343c1b044
Marcus Puchalla <marcus.puchalla@googlemail.com>
2024-08-30 11:31:01 +0200

    chore: corg dep

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
  '(package-selected-packages
-   '(exec-path-from-shell zoom-window zenburn-theme yasnippet winum which-key-posframe web-server vertico typescript-mode ts-fold treesit-auto spaceline smex pomm org-wc org-super-agenda org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard corfu consult-lsp company claude-shell chatgpt-shell cape anki-editor all-the-icons-dired all-the-icons-completion 0x0)))
+   '(corg jq-mode ob-http json-ls zoom-window yasnippet winum which-key web-server vertico typescript-mode ts-fold treesit-auto spaceline pomm org-wc org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu consult-lsp claude-shell chatgpt-shell cape anki-editor all-the-icons-dired all-the-icons-completion)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
