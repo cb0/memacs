90cde63a2f83091ff22e5e9de57b1b972b0e80ea
Marcus Puchalla <marcus.puchalla@googlemail.com>
2025-03-24 21:21:26 +0100

    fix: current working config
    Ready for refactor!

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,7 +4,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
- '(package-selected-packages '(vue-ts-mode)))
+ '(package-selected-packages
+   '(all-the-icons-completion all-the-icons-dired anki-editor arch-packer
+			      claude-shell consult-lsp corfu counsel
+			      dashboard desktop-environment
+			      embark-consult embark-org-roam emms enwc
+			      esup expand-region exwm flycheck focus
+			      gptel gruvbox-theme ido-completing-read+
+			      json-mode lsp-tailwindcss lsp-treemacs
+			      lsp-ui magit marginalia multiple-cursors
+			      nerd-icons npm-mode ob-http orderless
+			      org-caldav org-wc pomm spaceline
+			      sudo-edit system-packages
+			      treemacs-projectile treesit-auto ts-fold
+			      typescript-mode typst-ts-mode vertico
+			      vue-mode vue-ts-mode web-server winum
+			      zenburn-theme zoom-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
