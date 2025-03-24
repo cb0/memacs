e525cfe5117b872cb3414205331b1fa994ea3f17
Marcus Puchalla <marcus.puchalla@googlemail.com>
2025-03-24 21:19:36 +0100

    fix: startup problems in emacs 30

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (directory-files-recursively "~/sync/org/" "\\.org$"))
- '(package-selected-packages
-   '(vue-ts-mode vue-mode corg jq-mode ob-http json-ls zoom-window yasnippet winum which-key web-server vertico typescript-mode ts-fold treesit-auto spaceline pomm org-wc org-caldav orderless npm-mode nerd-icons multiple-cursors modus-themes marginalia magit lsp-ui lsp-treemacs lsp-tailwindcss json-mode focus flycheck exwm expand-region enwc emms embark-org-roam embark-consult desktop-environment dashboard counsel corfu consult-lsp claude-shell chatgpt-shell cape anki-editor all-the-icons-dired all-the-icons-completion)))
+ '(package-selected-packages '(vue-ts-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
